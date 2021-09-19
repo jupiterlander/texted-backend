243698d269432c4b569dc94886fc60015b0c66a5
jupiterlander <20988990+jupiterlander@users.noreply.github.com>
2021-09-19 19:00:12 +0200

    Bug fixes

--- database/config.js
+++ database/config.js
@@ -1,7 +1,6 @@
 const user = process.env.DB_USER
-const secret = process.env.SECRET
+const secret = process.env.DB_SECRET
 
-//const url = 'mongodb+srv://texted:8YwQlMPb5MctrL9n@cluster0.a67az.mongodb.net/texteditor?retryWrites=true&w=majority';
 const url = `mongodb+srv://${user}:${secret}@cluster0.a67az.mongodb.net/texteditor?retryWrites=true&w=majority`;
 const dbName = 'texteditor';
 const collectionName = 'documents';
--- database/config.test.js
+++ database/config.test.js
@@ -1,18 +1,8 @@
 const user = process.env.DB_USER
 const secret = process.env.DB_SECRET
 
-//const url = 'mongodb+srv://texted:8YwQlMPb5MctrL9n@cluster0.a67az.mongodb.net/texteditor?retryWrites=true&w=majority';
 const url = `mongodb+srv://${user}:${secret}@cluster0.a67az.mongodb.net/texteditor?retryWrites=true&w=majority`;
 const dbName = 'texteditor_test';
 const collectionName = 'documents';
 
 module.exports = { url, dbName, collectionName };
-
-// const url = 'mongodb+srv://texted_test:e1ZpjxAiocldFc36@cluster0.a67az.mongodb.net/texteditor?retryWrites=true&w=majority';
-// const dbName = 'texteditor_test';
-// const collectionName = 'documents';
-// const url = process.env.NODE_ENV;
-
-
-
-// module.exports = { url, dbName, collectionName };
