04ee91d159a0bcd95b7fdb462f5dfef664031f2b
jupiterlander <20988990+jupiterlander@users.noreply.github.com>
2021-09-19 18:20:18 +0200

    Minor change

--- database/config.test.js
+++ database/config.test.js
@@ -1,5 +1,5 @@
 const user = process.env.DB_USER
-const secret = process.env.SECRET
+const secret = process.env.DB_SECRET
 
 //const url = 'mongodb+srv://texted:8YwQlMPb5MctrL9n@cluster0.a67az.mongodb.net/texteditor?retryWrites=true&w=majority';
 const url = `mongodb+srv://${user}:${secret}@cluster0.a67az.mongodb.net/texteditor?retryWrites=true&w=majority`;
